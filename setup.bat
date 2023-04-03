7e74c9318daccc489c7a308ad591daf026210d66
Tuyen Truong <tuyen.truong@gmail.com>
2023-04-02 22:58:15 -0700

    Added Windows executables compiled using CMake

--- setup.bat
+++ setup.bat
@@ -5,8 +5,13 @@ conda run -n llama.cpp pip install torch numpy sentencepiece
 cmake .
 cmake --build . --config Release
 
-conda run -n llama.cpp python3 convert-pth-to-ggml.py ../data/llama/7B/ 1
-conda run -n llama.cpp ./quantize ../data/llama/7B/ggml-model-f16.bin ../data/llama/7B/ggml-model-q4_0.bin 2
+copy bin\Release\embedding.exe embedding.exe
+copy bin\Release\main.exe main.exe
+copy bin\Release\perplexity.exe perplexity.exe
+copy bin\Release\quantize.exe quantize.exe
 
-conda run -n llama.cpp python3 convert-pth-to-ggml.py ../data/llama/13B/ 1
-conda run -n llama.cpp ./quantize ../data/llama/13B/ggml-model-f16.bin ../data/llama/13B/ggml-model-q4_0.bin 2
+conda run -n llama.cpp python convert-pth-to-ggml.py ../data/llama/7B/ 1
+quantize ../data/llama/7B/ggml-model-f16.bin ../data/llama/7B/ggml-model-q4_0.bin 2
+
+conda run -n llama.cpp python convert-pth-to-ggml.py ../data/llama/13B/ 1
+quantize ../data/llama/13B/ggml-model-f16.bin ../data/llama/13B/ggml-model-q4_0.bin 2
