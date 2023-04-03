242ed28550c841a06563a872b9b6a48e4524d863
Tuyen Truong <tuyen.truong@gmail.com>
2023-04-02 22:24:08 -0700

    Added setup script for Windows

--- setup.bat
+++ setup.bat
@@ -1,10 +1,9 @@
-#!/bin/bash
-
 conda env remove -n llama.cpp -y
 conda env create -n llama.cpp --file environment.yaml
 conda run -n llama.cpp pip install torch numpy sentencepiece
 
-make
+cmake .
+cmake --build . --config Release
 
 conda run -n llama.cpp python3 convert-pth-to-ggml.py ../data/llama/7B/ 1
 conda run -n llama.cpp ./quantize ../data/llama/7B/ggml-model-f16.bin ../data/llama/7B/ggml-model-q4_0.bin 2
